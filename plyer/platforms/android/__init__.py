e1d0e4e3366eb00a4ba0d46e1f19f9ece82d04e5
Zdesl <39129214+Zdesl@users.noreply.github.com>
2021-11-19 05:41:44 +0100

    test change activity for service

--- plyer/platforms/android/__init__.py
+++ plyer/platforms/android/__init__.py
@@ -11,8 +11,8 @@ except (ImportError, AttributeError):
     ns = 'org.renpy.android'
 
 if 'PYTHON_SERVICE_ARGUMENT' in environ:
-    PythonService = autoclass(ns + '.PythonService')
-    activity = PythonService.mService
+    PythonActivity = autoclass(ns + '.PythonActivity')
+    activity = PythonActivity.mActivity
 else:
     PythonActivity = autoclass(ns + '.PythonActivity')
     activity = PythonActivity.mActivity
